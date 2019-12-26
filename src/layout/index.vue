c3edd5623c542f3e9e769bab8330bfb54a9ad59b
郑玉杰 <hzzhengyujie@corp.netease.com>
2019-12-26 11:59:29 +0800

    头部样式调整

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -2,27 +2,9 @@
   <div class="layout">
     <Layout :style="{ height: '100%' }">
       <Header>
-        <Menu mode="horizontal" theme="dark" active-name="1">
-          <div class="layout-logo"></div>
-          <div class="layout-nav">
-            <MenuItem name="1">
-              <Icon type="ios-navigate"></Icon>
-              Item 1
-            </MenuItem>
-            <MenuItem name="2">
-              <Icon type="ios-keypad"></Icon>
-              Item 2
-            </MenuItem>
-            <MenuItem name="3">
-              <Icon type="ios-analytics"></Icon>
-              Item 3
-            </MenuItem>
-            <MenuItem name="4">
-              <Icon type="ios-paper"></Icon>
-              Item 4
-            </MenuItem>
-          </div>
-        </Menu>
+        <div class="layout-logo">
+          <img src="./logo2@2x.png" class="logo" />
+        </div>
       </Header>
       <Layout :style="{ height: '100%' }">
         <Sider hide-trigger width="180" class="sider-style">
@@ -44,7 +26,7 @@ export default {
   }
 };
 </script>
-<style scoped>
+<style lang="less" scoped>
 .layout {
   background: #f5f7f9;
   position: relative;
@@ -52,15 +34,19 @@ export default {
   height: 100%;
 }
 .layout-logo {
-  width: 100px;
-  height: 30px;
-  background: #5b6270;
-  border-radius: 3px;
-  float: left;
-  position: relative;
-  top: 15px;
-  left: 20px;
+  width: 180px;
+  height: 64px;
+  position: absolute;
+  text-align: center;
+  padding: 9px 10px 9px 0;
+  background: #2a313c;
+  z-index: 10;
+  .logo {
+    width: 105px;
+    height: 32px;
+  }
 }
+.ivu-layout-header{padding: 0;background:#fff;}
 .sider-style {
   overflow: auto;
   background: #2a313c;
